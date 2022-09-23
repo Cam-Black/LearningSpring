c2ae128bc761c08aeaaf0ebd47c9029c63d1ba2b
Cam-Black <cameronablack1998@gmail.com>
2022-09-23 11:01:27 +0100

    Added Autowired annotation to constructor to explicitly tell compiler to inject the dependencies in the constructor.

--- src/main/java/com/qa/learningspring/student/controller/StudentController.java
+++ src/main/java/com/qa/learningspring/student/controller/StudentController.java
@@ -1,22 +1,26 @@
 package com.qa.learningspring.student.controller;
 
 import com.qa.learningspring.student.entity.Student;
+import com.qa.learningspring.student.service.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.time.LocalDate;
 import java.util.List;
 
 @RestController
+@RequestMapping(path = "/student")
 public class StudentController {
+	private final StudentService service;
+	
+	@Autowired
+	public StudentController(StudentService service) {
+		this.service = service;
+	}
+	
 	@GetMapping
 	public List<Student> getStudents() {
-		return List.of(
-				new Student(
-						1L,
-						"Cam",
-						"camblack",
-						LocalDate.of(1998, 3, 18),
-						18));
+		return this.service.listEntities();
 	}
 }
